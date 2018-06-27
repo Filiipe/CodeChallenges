64c2eb5233f1dee5479ad649b9ea44e0f552b1f6
Filiipe <filipessm@live.com>
2018-06-27 15:50:22 -0300

    Factorialize a Number solution

--- BasicAlgorithm/BasicAlgorithms.js
+++ BasicAlgorithm/BasicAlgorithms.js
@@ -1,13 +1,27 @@
-// Convert Celsius to Fahrenheit
+// ---- Convert Celsius to Fahrenheit ---- //
 
 function convertToF(celsius) {
-  let fahrenheit = (9/5 * celsius) + 32;
+  let fahrenheit = (9 / 5 * celsius) + 32;
   return fahrenheit;
 }
 console.log(`fahrenheit: ${convertToF(30)}`);
 
-// Reverse a String
+// ---- Reverse a String ---- //
+
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 console.log(`Hello reversed: ${reverseString("hello")}`);
+
+// ---- Factorialize a Number ---- //
+
+function factorialize(num) {
+  if (num === 0)
+    return 1;
+
+  for (let i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+  return num;
+}
+console.log(`Factorization of 5: ${factorialize(5)}`);
